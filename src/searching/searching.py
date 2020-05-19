d34956bfe5e06ca221c9a0fef9b37f21ba732490
marielapg <mpgonzales77@gmail.com>
2020-05-19 09:35:53 -0700

    searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,32 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    # for i in range(len(arr)):
+	#     if arr[i]==target:
+	# 	return i
+    # return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr) -1
 
-
+    while left <= right:
+        #find the midpoint 
+        mid = (left + right) // 2
+        #check to see if the midpoint element is our target 
+        if arr[mid] == target:
+            return mid
+        #check to see if the element should be on the left or right side of our midpoint
+        if arr[mid] < target:
+            #toss out the left side of the arr
+            #update our 'left' index
+            left = mid +1 
+        #otherwise, arr[mid] > target 
+        else:
+            # toss out the right side of the arr because the element has to be on the left side 
+            right = mid -1
+​
     return -1  # not found
